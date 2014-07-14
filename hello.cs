625e4541479fb94017c6b5327fd1889c92b4c1c8
dsaraf@tavisca.com <dsaraf@tavisca.com>
2014-07-14 23:21:25 +0530

    Added many Console Statements

--- hello.cs
+++ hello.cs
@@ -5,11 +5,12 @@ static void main(string[] args)
 
 	
 	Console.WriteLine("Hey this is darpan saraf.!!!");
+	Console.WriteLine("i like this song");
+	Console.WriteLine("i like this music");
+	Console.WriteLine("i like this shoes");
+	Console.WriteLine("i like this food");
+
 	//Added a first comment
 	//Added a second comment
-	I am at home
-	this is my home repository 
-	i am not new to this
-	i have added some lines
-
+	//
 }
